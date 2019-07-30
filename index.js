4171fec4afba7a15405e4da995d0a503ca6bb13f
Justen Robertson <justen@justen.us>
2019-07-30 14:30:53 -0700

    feat: add system channel greeting message

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const commands = require('./lib/commands')
 client.once('ready', () => {
   console.log('Ready!')
   console.log(`Add me at https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8192`)
+  Promise.all([...client.guilds.values()].map(guild => {
+    if (guild.systemChannel) return guild.systemChannel.send('I\'m ready to roll! try `/clattr help` for usage.')
+    return Promise.resolve()
+  }))
 })
 
 client.on('message', async (message) => {
